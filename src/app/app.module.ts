30dcc1e12f27fa45315fb4f038d50228da4dc508
Bogdan Arsenic <bogdanarsenic@gmail.com>
2019-08-31 20:36:06 +0200

    bzv

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,31 +3,21 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing/app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './core/header/header.component';
 import { NavbarComponent } from './core/navbar/navbar.component';
-import { EditWineComponent } from './wine/edit-wine/edit-wine.component';
-import { PaginationComponent } from './wine/pagination/pagination.component';
-import { WineListComponent } from './wine/wine-list/wine-list.component';
-import { SearchFormComponent } from './wine/search-form/search-form.component';
-import { TableComponent } from './wine/table/table.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     NavbarComponent,
-    EditWineComponent,
-    PaginationComponent,
-    WineListComponent,
-    SearchFormComponent,
-    TableComponent
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule
--- src/app/register/register.component.spec.ts
+++ src/app/register/register.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SearchFormComponent } from './search-form.component';
+import { RegisterComponent } from './register.component';
 
-describe('SearchFormComponent', () => {
-  let component: SearchFormComponent;
-  let fixture: ComponentFixture<SearchFormComponent>;
+describe('RegisterComponent', () => {
+  let component: RegisterComponent;
+  let fixture: ComponentFixture<RegisterComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SearchFormComponent ]
+      declarations: [ RegisterComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(SearchFormComponent);
+    fixture = TestBed.createComponent(RegisterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
